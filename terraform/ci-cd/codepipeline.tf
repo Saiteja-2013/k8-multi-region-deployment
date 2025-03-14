077379fda4f310e9354d9ca8b9fa787e3f7d1cc6
Saiteja <93371297+Saiteja-2013@users.noreply.github.com>
2025-03-14 09:23:43 +0530

    Create codepipeline.tf

---
+++ terraform/ci-cd/codepipeline.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_codepipeline" "web_app_pipeline" {
+  name     = "web-app-codepipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.pipeline_artifacts.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = var.github_branch
+        OAuthToken = var.github_oauth_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.web_app_build.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      version         = "1"
+      input_artifacts = ["build_output"]
+
+      configuration = {
+        ApplicationName     = aws_codedeploy_app.web_app_deploy.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.web_app_deploy_group.deployment_group_name
+      }
+    }
+  }
+}
