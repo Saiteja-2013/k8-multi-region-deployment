525bda238ba1baa7e0359b032b5ec6a727ce1c22
Saiteja <93371297+Saiteja-2013@users.noreply.github.com>
2025-03-14 09:30:07 +0530

    Create iam-roles.tf

---
+++ terraform/security/iam-roles.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "codepipeline_role" {
+  name = "CodePipelineServiceRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
